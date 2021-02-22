93ad4cd346f852696eaf7b9fbaf8d6b050d4d451
Xiaofeng Cai <xfcai89@126.com>
2021-02-22 17:33:43 -0500

    Add files via upload

---
+++ RKDG/module_gauss_tri.f90
@@ -0,0 +1,254 @@
+    !*************************************************************************
+    module module_gauss_tri
+    ! the number of gauss quadrature points was set in module_prob
+    use module_prob,only : ng_tri ! the number of gauss quadrature points!
+    implicit none
+
+    real,allocatable :: w(:),sx(:),sy(:)
+    !!************************************************
+    !! 6 quadrature points that is exact for polynomials
+    !! of total degree less than 4
+    !! which can be found in page of book
+    !! Discontinuous Galerkin Methods for solving Elliptic and Parabolic
+    !! Equations: Theory and Implementation.
+    !REAL, PARAMETER, DIMENSION(1:6) :: w = (/ &
+    !    0.11169079483901,&
+    !    0.11169079483901,&
+    !    0.11169079483901,&
+    !    0.05497587182766,&
+    !    0.05497587182766,&
+    !    0.05497587182766/)
+    !
+    !REAL, PARAMETER, DIMENSION(1:6) :: sx = (/ &
+    !    0.445948490915965,&
+    !    0.108103018168070,&
+    !    0.445948490915965,&
+    !    0.091576213509771,&
+    !    0.816847572980459,&
+    !    0.091576213509771/)
+    !
+    !REAL, PARAMETER, DIMENSION(1:6) :: sy = (/ &
+    !    0.445948490915965,&
+    !    0.445948490915965,&
+    !    0.108103018168070,&
+    !    0.091576213509771,&
+    !    0.091576213509771,&
+    !    0.816847572980459/)
+    contains
+    !*************************************************************************
+    ! quadrature rules for triangles.
+    !
+    ! quadrature rules for the triangle are generally defined over T1,
+    ! the unit triangle whose vertices are (0,0), (1,0), (0,1).
+    !
+    ! from
+    !http://people.sc.fsu.edu/~jburkardt/datasets/quadrature_rules_tri/quadrature_rules_tri.html
+    !
+    ! References:
+    ! 1. Jarle Berntsen, Terje Espelid,
+    ! Algorithm 706,
+    ! DCUTRI: an algorithm for adaptive cubature over a collection
+    ! of triangles,
+    ! ACM Transactions on Mathematical Software,
+    ! Volume 18, Number 3, September 1992, pages 329-342.
+    !
+    ! 2. Elise deDoncker, Ian Robinson,
+    ! Algorithm 612: Integration over a Triangle
+    ! Using Nonlinear Extrapolation,
+    ! ACM Transactions on Mathematical Software,
+    ! Volume 10, Number 1, March 1984, pages 17-22.
+    !
+    ! 3. Dirk Laurie,
+    ! Algorithm 584, CUBTRI, Automatic Cubature Over a Triangle,
+    ! ACM Transactions on Mathematical Software,
+    ! Volume 8, Number 2, 1982, pages 210-218.
+    !
+    ! 4. James Lyness, Dennis Jespersen,
+    ! Moderate Degree Symmetric Quadrature Rules for the Triangle,
+    ! Journal of the Institute of Mathematics and its Applications,
+    ! Volume 15, Number 1, February 1975, pages 19-32.
+    !
+    ! 5. Hans Rudolf Schwarz,
+    ! Finite Element Methods,
+    ! Academic Press, 1988,
+    ! ISBN: 0126330107,
+    ! LC: TA347.F5.S3313.
+    !
+    ! 6. Gilbert Strang, George Fix,
+    ! An Analysis of the Finite Element Method,
+    ! Cambridge, 1973,
+    ! ISBN: 096140888X,
+    ! LC: TA335.S77.
+    !
+    ! 7. Arthur Stroud,
+    ! Approximate Calculation of Multiple Integrals,
+    ! Prentice Hall, 1971,
+    ! ISBN: 0130438936,
+    ! LC: QA311.S85.
+    !
+    ! 8. Olgierd Zienkiewicz,
+    ! The Finite Element Method,
+    ! Sixth Edition,
+    ! Butterworth-Heinemann, 2005,
+    ! ISBN: 0750663200,
+    ! LC: TA640.2.Z54
+    ! 9. David Dunavant,
+    ! High Degree Efficient Symmetrical Gaussian Quadrature Rules
+    ! for the Triangle,
+    ! International Journal for Numerical Methods in Engineering,
+    ! Volume 21, 1985, pages 1129-1148.
+    !*************************************************************************
+    subroutine quadrature_rules_tri( ng_tri,w,sx,sy )
+    implicit none
+    integer,intent(in) :: ng_tri
+    real,intent(out) :: w(ng_tri),sx(ng_tri),sy(ng_tri)
+
+
+
+    if( ng_tri==1 )then
+        ! degree of precision 1.
+        w = (/  0.5 /)
+        sx = (/  1./3. /)
+        sy = (/  1./3. /)
+    elseif( ng_tri==6 )then
+        !************************************************
+        ! 6 quadrature points that is exact for polynomials
+        ! of total degree up to 4
+        ! which can be found in page of book
+        ! Discontinuous Galerkin Methods for solving Elliptic and Parabolic
+        ! Equations: Theory and Implementation.
+        !
+        ! David Dunavant,
+        ! High Degree Efficient Symmetrical Gaussian Quadrature Rules
+        ! for the Triangle,
+        ! International Journal for Numerical Methods in Engineering,
+        ! Volume 21, 1985, pages 1129-1148.
+        w = (/ &
+            0.11169079483901,&
+            0.11169079483901,&
+            0.11169079483901,&
+            0.05497587182766,&
+            0.05497587182766,&
+            0.05497587182766/)
+
+        sx = (/ &
+            0.445948490915965,&
+            0.108103018168070,&
+            0.445948490915965,&
+            0.091576213509771,&
+            0.816847572980459,&
+            0.091576213509771/)
+
+        sy = (/ &
+            0.445948490915965,&
+            0.445948490915965,&
+            0.108103018168070,&
+            0.091576213509771,&
+            0.091576213509771,&
+            0.816847572980459/)
+    elseif( ng_tri==9 )then
+        ! degree of precision 6.
+        w = (/ &
+            0.205950504760887, &
+            0.205950504760887, &
+            0.205950504760887, &
+            0.063691414286223, &
+            0.063691414286223, &
+            0.063691414286223, &
+            0.063691414286223, &
+            0.063691414286223, &
+            0.063691414286223 /)
+        w = 0.5*w
+
+        sx = (/ &
+            0.124949503233232, &
+            0.437525248383384, &
+            0.437525248383384, &
+            0.797112651860071, &
+            0.797112651860071, &
+            0.165409927389841, &
+            0.165409927389841, &
+            0.037477420750088, &
+            0.037477420750088 /)
+
+        sy = (/ &
+            0.437525248383384, &
+            0.124949503233232, &
+            0.437525248383384, &
+            0.165409927389841, &
+            0.037477420750088, &
+            0.797112651860071, &
+            0.037477420750088, &
+            0.797112651860071,&
+            0.165409927389841         /)
+    elseif( ng_tri==13 )then
+        ! degree of precision 7.
+        w = (/ &
+            -0.149570044467670 ,&
+            0.175615257433204 ,&
+            0.175615257433204 ,&
+            0.175615257433204 ,&
+            0.053347235608839 ,&
+            0.053347235608839 ,&
+            0.053347235608839 ,&
+            0.077113760890257 ,&
+            0.077113760890257 ,&
+            0.077113760890257 ,&
+            0.077113760890257 ,&
+            0.077113760890257 ,&
+            0.077113760890257       /)
+        w = 0.5*w
+
+        sx =(/ &
+            0.333333333333333 ,&
+            0.479308067841923 ,&
+            0.260345966079038 ,&
+            0.260345966079038 ,&
+            0.869739794195568 ,&
+            0.065130102902216 ,&
+            0.065130102902216 ,&
+            0.638444188569809 ,&
+            0.638444188569809 ,&
+            0.312865496004875 ,&
+            0.312865496004875 ,&
+            0.048690315425316 ,&
+            0.048690315425316 /)
+
+        sy = (/ &
+            0.333333333333333 ,&
+            0.260345966079038 ,&
+            0.479308067841923 ,&
+            0.260345966079038 ,&
+            0.065130102902216 ,&
+            0.869739794195568 ,&
+            0.065130102902216 ,&
+            0.312865496004875 ,&
+            0.048690315425316 ,&
+            0.638444188569809 ,&
+            0.048690315425316 ,&
+            0.638444188569809 ,&
+            0.312865496004875 /)
+
+    endif
+    end subroutine quadrature_rules_tri
+    !*************************************************************************
+    subroutine allocate_gau_para
+    implicit none
+
+
+    allocate( w(ng_tri) )
+    allocate( sx(ng_tri) )
+    allocate( sy(ng_tri) )
+
+    end subroutine allocate_gau_para
+    !*************************************************************************
+    subroutine deallocate_gau_para
+    implicit none
+
+    deallocate( w )
+    deallocate( sx )
+    deallocate( sy )
+
+    end subroutine deallocate_gau_para
+    !*************************************************************************
+    end module module_gauss_tri
---
+++ RKDG/term_volume.f90
@@ -0,0 +1,80 @@
+    subroutine term_volume(io,tt)
+    use module_polynomials_tri,only : dxh_ortho_basis_tri
+    use module_polynomials_tri,only : dyh_ortho_basis_tri
+    use module_polynomials_tri,only : polyt
+    implicit none
+    integer,intent(in) :: io
+    real,intent(in) :: tt
+
+    integer :: iele,k,ig
+    real :: test1,test2,temp
+    real :: gau(2),pc(1:2),ar,oa(1:5,1:5)
+    real :: ff,gg, uu
+    real :: ainv(1:2,1:2)
+    real :: t1,t2
+
+    real :: xh,yh
+    integer :: n1,n2,n3
+    real :: x1,y1,x2,y2,x3,y3
+
+
+    do iele = 1, n_element
+
+        ainv(1,1) = element(iele)%jac_inv(1,1)
+        ainv(1,2) = element(iele)%jac_inv(1,2)
+        ainv(2,1) = element(iele)%jac_inv(2,1)
+        ainv(2,2) = element(iele)%jac_inv(2,2)
+
+        n1 = element( iele )%node(1)
+        n2 = element( iele )%node(2)
+        n3 = element( iele )%node(3)
+
+        x1 = node(n1)%coor(1)
+        y1 = node(n1)%coor(2)
+
+        x2 = node(n2)%coor(1)
+        y2 = node(n2)%coor(2)
+
+        x3 = node(n3)%coor(1)
+        y3 = node(n3)%coor(2)
+        !****************************************
+        ! for calculating the polynomial
+        pc(1:2) = element(iele)%bary(1:2)
+        ar = element(iele)%area
+        oa(1:5,1:5) = element(iele)%a(1:5,1:5)
+        !****************************************
+        do k = 1 , n_moment
+
+            temp = 0.
+            do ig = 1, ng_tri
+                gau(1:2) = element(iele)%gauss(ig,1:2)
+
+
+                !***********************
+                xh = sx(ig)
+                yh = sy(ig)
+
+                !test1 = dx_ortho_basis_tri(k,gau(1),gau(2),x0,y0,ar,oa(1:5,1:5))
+                !test2 = dy_ortho_basis_tri(k,gau(1),gau(2),x0,y0,ar,oa(1:5,1:5))
+
+                test1 = dxh_ortho_basis_tri(k,xh,yh,pc(1:2),ar,oa(1:5,1:5),x1,y1,x2,y2,x3,y3 )
+                test2 = dyh_ortho_basis_tri(k,xh,yh,pc(1:2),ar,oa(1:5,1:5),x1,y1,x2,y2,x3,y3 )
+
+                !Be^transpose
+
+                t1 = ainv(1,1)*test1+ainv(2,1)*test2
+                t2 = ainv(1,2)*test1+ainv(2,2)*test2
+
+                uu = polyt( n_moment,element(iele)%umodal(1:n_moment,io),gau(1:2),pc(1:2),ar,oa(1:5,1:5) )
+
+                ff = fun_f( uu,gau(1:2),tt )
+                gg = fun_g( uu,gau(1:2),tt )
+                temp = temp + (ff*t1+gg*t2)*w(ig) *ar *2.
+     
+            enddo
+            volume(iele,k) = temp
+
+        enddo
+    enddo
+
+    end subroutine term_volume
